013c1741fdda3e2692070b046e356fae65bb0f44
taichi98 <hoangthong012@gmail.com>
2025-01-11 09:39:58 +0700

    Update and rename test.php to header.php

---
+++ header.php
@@ -0,0 +1,13 @@
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet"/>
+    <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css"/>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/themes/material_green.css"/>
+    <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+    <link rel="stylesheet" href="/style.css" />
+    <script src="/script.js"></script>
+    <title><?php echo $pageTitle ?? 'ICU Tools'; ?></title>
+    <link rel="icon" href="/icon.png" type="image/png" />
+</head>
