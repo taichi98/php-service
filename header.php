3c3cccc87821045094a58fccbc5582cd0c6f3dc3
taichi98 <hoangthong012@gmail.com>
2025-01-11 11:50:48 +0700

    Update header.php

--- header.php
+++ header.php
@@ -2,12 +2,30 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet"/>
-    <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css"/>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/themes/material_green.css"/>
-    <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
     <title><?php echo $pageTitle ?? 'ICU Tools'; ?></title>
     <link rel="icon" href="/icon.png" type="image/png">
     <link rel="stylesheet" href="/style.css" />
+    <?php
+    // Thêm style riêng cho từng trang
+    if (isset($page)) {
+        switch ($page) {
+            case 'cpap':
+                echo '<link rel="stylesheet" href="/cpap.css">';
+                break;
+            case 'ett':
+                echo '<link rel="stylesheet" href="/ett.css">';
+                break;
+            case 'bmi':
+                echo '<link rel="stylesheet" href="/bmi.css">';
+                break;
+            case 'zscore-calculator':
+                echo '<script src="https://cdn.plot.ly/plotly-latest.min.js"></script>';
+                echo '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css"/>'
+                echo '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/themes/material_green.css"/>'
+                echo '<script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>'
+                break;
+        }
+    }
+    ?>
     <script src="/script.js"></script>
 </head>
