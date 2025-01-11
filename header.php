57ebda096e3043d6408b6c7d199de2a15c38d7b7
taichi98 <hoangthong012@gmail.com>
2025-01-11 11:53:57 +0700

    Update header.php

--- header.php
+++ header.php
@@ -20,9 +20,9 @@
                 break;
             case 'zscore-calculator':
                 echo '<script src="https://cdn.plot.ly/plotly-latest.min.js"></script>';
-                echo '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css"/>'
-                echo '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/themes/material_green.css"/>'
-                echo '<script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>'
+                echo '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css"/>';
+                echo '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/themes/material_green.css"/>';
+                echo '<script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>';
                 break;
         }
     }
