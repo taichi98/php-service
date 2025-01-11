3396cf6ec4972671fc87e890cb2c5d89927ae07c
taichi98 <hoangthong012@gmail.com>
2025-01-11 11:04:38 +0700

    Update 404.php

--- 404.php
+++ 404.php
@@ -12,77 +12,107 @@
     <title>404 Page Not Found</title>
 
     <style>
-        @import url('https://fonts.googleapis.com/css?family=Cabin:400,700');
-        @import url('https://fonts.googleapis.com/css?family=Montserrat:900');
-
+        * {
+          -webkit-box-sizing: border-box;
+                  box-sizing: border-box;
+        }
+        
         body {
-            padding: 0;
-            margin: 0;
+          padding: 0;
+          margin: 0;
         }
-
+        
         #notfound {
-            position: relative;
-            height: 100vh;
-            background: #030005;
+          position: relative;
+          height: 100vh;
         }
-
+        
         #notfound .notfound {
-            position: absolute;
-            left: 50%;
-            top: 50%;
-            transform: translate(-50%, -50%);
+          position: absolute;
+          left: 50%;
+          top: 50%;
+          -webkit-transform: translate(-50%, -50%);
+              -ms-transform: translate(-50%, -50%);
+                  transform: translate(-50%, -50%);
         }
-
+        
         .notfound {
-            max-width: 520px;
-            width: 100%;
-            text-align: center;
-            line-height: 1.4;
+          max-width: 520px;
+          width: 100%;
+          line-height: 1.4;
+          text-align: center;
         }
-
+        
         .notfound .notfound-404 {
-            position: relative;
-            height: 200px;
+          position: relative;
+          height: 240px;
         }
-
+        
         .notfound .notfound-404 h1 {
-            font-family: 'Montserrat', sans-serif;
-            position: absolute;
-            left: 50%;
-            top: 50%;
-            transform: translate(-50%, -50%);
-            font-size: 220px;
-            font-weight: 900;
-            margin: 0;
-            background: url('https://colorlib.com/etc/404/colorlib-error-404-10/bg.jpg') no-repeat;
-            background-clip: text;
-            -webkit-background-clip: text;
-            color: transparent;
-            text-transform: uppercase;
+          font-family: 'Montserrat', sans-serif;
+          position: absolute;
+          left: 50%;
+          top: 50%;
+          -webkit-transform: translate(-50%, -50%);
+              -ms-transform: translate(-50%, -50%);
+                  transform: translate(-50%, -50%);
+          font-size: 252px;
+          font-weight: 900;
+          margin: 0px;
+          color: #262626;
+          text-transform: uppercase;
+          letter-spacing: -40px;
+          margin-left: -20px;
         }
-
+        
+        .notfound .notfound-404 h1>span {
+          text-shadow: -8px 0px 0px #fff;
+        }
+        
         .notfound .notfound-404 h3 {
-            font-family: 'Cabin', sans-serif;
-            position: absolute;
-            left: 0;
-            right: 0;
-            top: 110%;
-            font-size: 16px;
-            font-weight: 400;
-            color: #fff;
-            text-transform: uppercase;
-            margin: 0;
+          font-family: 'Cabin', sans-serif;
+          position: relative;
+          font-size: 16px;
+          font-weight: 700;
+          text-transform: uppercase;
+          color: #262626;
+          margin: 0px;
+          letter-spacing: 3px;
+          padding-left: 6px;
         }
-
+        
         .notfound h2 {
-            font-family: 'Cabin', sans-serif;
-            font-size: 20px;
-            font-weight: 400;
-            text-transform: uppercase;
-            color: #fff;
-            margin-top: 0;
+          font-family: 'Cabin', sans-serif;
+          font-size: 20px;
+          font-weight: 400;
+          text-transform: uppercase;
+          color: #000;
+          margin-top: 0px;
+          margin-bottom: 25px;
         }
-
+        
+        @media only screen and (max-width: 767px) {
+          .notfound .notfound-404 {
+            height: 200px;
+          }
+          .notfound .notfound-404 h1 {
+            font-size: 200px;
+          }
+        }
+        
+        @media only screen and (max-width: 480px) {
+          .notfound .notfound-404 {
+            height: 162px;
+          }
+          .notfound .notfound-404 h1 {
+            font-size: 162px;
+            height: 150px;
+            line-height: 162px;
+          }
+          .notfound h2 {
+            font-size: 16px;
+          }
+        }
     </style>
 </head>
 
