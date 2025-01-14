e144d9117b0b412a71df8d061f545fb22917876d
taichi98 <hoangthong012@gmail.com>
2025-01-14 10:54:50 +0700

    Update zscore-calculator.php

--- zscore-calculator.php
+++ zscore-calculator.php
@@ -6,7 +6,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         'ageInDays' => $_POST['ageInDays'],
         'height' => $_POST['height'],
         'weight' => $_POST['weight'],
-        'measure' => $_POST['measure']
+        'measured' => $_POST['measured']
     ];
 
     $options = [
@@ -61,7 +61,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 </div>
 
                 <label for="measured">Measured:</label>
-                <select name="measure" id="measured" required>
+                <select name="measured" id="measured" required>
                     <option value="h">Standing</option>
                     <option value="l">Recumbent</option>
                 </select>
