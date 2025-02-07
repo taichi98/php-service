e3e540a679ad49f63ae816203b082a91ad79063f
taichi98 <hoangthong012@gmail.com>
2025-02-07 15:56:33 +0700

    Update header.php

--- header.php
+++ header.php
@@ -1,6 +1,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="google-site-verification" content="i6tfxtIdDxffxiWKBEo3snmUCl6GIBGRq4ikcW16KfI" />
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet"/>
     <title><?php echo $pageTitle ?? 'ICU Tools'; ?></title>
     <link rel="icon" href="/icon.png" type="image/png">
